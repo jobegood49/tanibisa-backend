1682c5538b7be8355a18fcfa6110763ec463fd08
Jonathan Nicolas <jonathan.s.nicolas@gmail.com>
2019-03-14 19:53:19 +0700

    Add new product into commidity

--- middlewares/products/controller.js
+++ middlewares/products/controller.js
@@ -1,5 +1,6 @@
 const Product = require('./model')
 const jwt = require('jsonwebtoken')
+const Commodity = require('../commodities/model')
 
 const controller = {
   //////////////////////////////////////////////////////////////////////////////
@@ -10,7 +11,7 @@ const controller = {
 
     res.status(200).send({
       message: 'List of all products',
-      products: products
+      products: products,
     })
   },
 
@@ -19,20 +20,30 @@ const controller = {
     const newProduct = {
       farmer_id: req.body.farmer_id,
       commodity_id: req.body.commodity_id,
-      price: req.body.price
+      price: req.body.price,
     }
 
     const result = await Product.create(newProduct)
+    const newCommodity = await Commodity.findOneAndUpdate(
+      {
+        _id: newProduct.commodity_id,
+      },
+      {
+        $push: {
+          products: result._id,
+        },
+      }
+    )
 
     res.status(200).send({
       message: 'Created new product',
-      result: result
+      result: result,
     })
   },
   ///////////////////////////////////////////////////////////////////////////////
   getOneProductById: async (req, res, next) => {
     const product = await Product.findOne({
-      id: Number(req.params.id)
+      id: Number(req.params.id),
     })
       .populate('farmer_id', { salt: 0, password: 0, email: 0 })
       .populate('commodity_id')
@@ -40,14 +51,14 @@ const controller = {
     if (product) {
       res.status(200).send({
         message: 'Get one product by id',
-        product: product
+        product: product,
       })
     } else {
       res.status(404).send({
-        message: 'Product not found!'
+        message: 'Product not found!',
       })
     }
-  }
+  },
 }
 
 module.exports = controller
