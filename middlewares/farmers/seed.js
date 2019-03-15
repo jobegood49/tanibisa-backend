e813a943a3ac40c06485085fc08fdc392b5ce44a
Muhammad Fahrianda <mfahrianda@gmail.com>
2019-03-15 16:24:05 +0700

    add farmer seed

---
+++ middlewares/farmers/seed.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    name: 'Agus Riyadi',
+    image:
+      'https://account.8villages.com/uploads/images/19/1532004801_xu100xgk3xr.jpg',
+    location: 'Kabupaten Magelang',
+
+    products: 'https://regopantes.imgix.net/assets//version2.5/yur-2.png'
+  },
+  {
+    name: 'Nina T. Rumawas',
+    image:
+      'https://account.8villages.com/uploads/images/13/1531455640_em6n4zyf1or.jpg',
+    location: 'Kota Batu',
+    products: 'https://regopantes.imgix.net/assets//version2.5/buah.png'
+  },
+  {
+    name: 'Serenity Farm',
+    image: 'Menthik wangi rice, Black rice, and Red rice with organic quality.',
+    location: 'Kabupaten Bandung Barat',
+    products:
+      'https://account.8villages.com/uploads/images/28/1548665413_zki9r5ghkt9.jpg'
+  }
+]
