807daaea904d1bd52ac9f0ecdccc74391c843955
M Haidar Hanif <git@mhaidarhanif.com>
2019-03-15 13:59:04 +0700

    Hotfix push new product into farmer's products

--- middlewares/products/controller.js
+++ middlewares/products/controller.js
@@ -1,6 +1,8 @@
 const Product = require('./model')
 const jwt = require('jsonwebtoken')
+
 const Commodity = require('../commodities/model')
+const Farmer = require('../farmers/model')
 
 const controller = {
   //////////////////////////////////////////////////////////////////////////////
@@ -11,7 +13,7 @@ const controller = {
 
     res.status(200).send({
       message: 'List of all products',
-      products: products,
+      products: products
     })
   },
 
@@ -20,30 +22,35 @@ const controller = {
     const newProduct = {
       farmer_id: req.body.farmer_id,
       commodity_id: req.body.commodity_id,
-      price: req.body.price,
+      price: req.body.price
     }
 
-    const result = await Product.create(newProduct)
+    // 1. create new product first
+    const newProduct = await Product.create(newProduct)
+
+    // 2. push new product into farmer's products
+    const newFarmer = await Farmer.findOneAndUpdate(
+      { _id: farmer_id },
+      { $push: { products: result_.id } }
+    )
+
+    // 3. push new product into commodity's products
     const newCommodity = await Commodity.findOneAndUpdate(
-      {
-        _id: newProduct.commodity_id,
-      },
-      {
-        $push: {
-          products: result._id,
-        },
-      }
+      { _id: newProduct.commodity_id },
+      { $push: { products: result_.id } }
     )
 
     res.status(200).send({
       message: 'Created new product',
-      result: result,
+      newProduct: newProduct,
+      newFarmer: newFarmer,
+      newCommodity: newCommodity
     })
   },
   ///////////////////////////////////////////////////////////////////////////////
   getOneProductById: async (req, res, next) => {
     const product = await Product.findOne({
-      id: Number(req.params.id),
+      id: Number(req.params.id)
     })
       .populate('farmer_id', { salt: 0, password: 0, email: 0 })
       .populate('commodity_id')
@@ -51,14 +58,14 @@ const controller = {
     if (product) {
       res.status(200).send({
         message: 'Get one product by id',
-        product: product,
+        product: product
       })
     } else {
       res.status(404).send({
-        message: 'Product not found!',
+        message: 'Product not found!'
       })
     }
-  },
+  }
 }
 
 module.exports = controller
